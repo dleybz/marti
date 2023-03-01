8a98185a644edefeca202fdf4ad673ebd12fdad4
Daniel David Leybzon <dannyleybzon@gmail.com>
2023-03-01 16:17:52 -0300

    Fixed a typo in the main() function

--- marti.py
+++ marti.py
@@ -6,7 +6,7 @@ import speak
 
 def main():
     """main function which acts as an entrypoint into the program. Tests if the user has said "finish conversation", exits if they have, otherwise responds to what they said"""
-    while ():
+    while True:
         user_input = hear.user_input()
         if "finish conversation" not in user_input:
             response = think.response(user_input)
